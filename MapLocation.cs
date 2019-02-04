2a66ab184b1019a65cdeb5e98a7edf3f37431165
Stephen Fowles-White <FowlesWhite@gmail.com>
2019-02-04 21:53:06 +0000

    inheritance of Point.cs and part of the Game.cs

---
+++ MapLocation.cs
@@ -0,0 +1,22 @@
+using System;
+namespace TreehouseDefense
+  {
+ 
+  //This class use inheritance of the Point.cs file.
+  class MapLocation : Point {
+    
+    //This constructor class is calling the same vaiable as the Point class and also passing through the same parametors of the Point Class (:base(x, y)
+    public MapLocation(int x, int y ): base(x, y)
+    {
+      
+      
+    }
+    
+    
+  }
+  
+  
+  
+  
+  
+}

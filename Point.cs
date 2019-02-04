f61ecce19ce0e3c286847413239b5e24b2b437fb
Stephen Fowles-White <FowlesWhite@gmail.com>
2019-02-04 20:15:14 +0000

    Part of Game.cs

---
+++ Point.cs
@@ -0,0 +1,22 @@
+namespace TreehouseDefense
+{
+  class Point {
+    //needs an x and a y pareneter
+              
+                public readonly int X;
+                public readonly int Y;
+                
+   //constructor class
+                public Point(int x, int y){
+                  X = x;
+                  Y = y;
+                                     
+                }
+    
+            
+    
+               }
+  //end of class
+  
+}
+//end of namespace
